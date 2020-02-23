80912c70e7ba1882e477a719a995ae6e8002fb0b
Albertoice <alberto@MacBook-Air-di-Alberto.local>
2020-02-23 19:17:58 +0100

    Added icons

---
+++ dist/precache-manifest.0ac3e0f286ad8f538b071b8c71ce1b20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590fa4f30105d7ee7698",
+    "url": "/css/app.6385aaf8.css"
+  },
+  {
+    "revision": "c0d37accae55c7e24d1ed2661dac7a2c",
+    "url": "/img/background.c0d37acc.jpg"
+  },
+  {
+    "revision": "6400d017c2906d02f16ef35a117f0c2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "590fa4f30105d7ee7698",
+    "url": "/js/app.19824a69.js"
+  },
+  {
+    "revision": "617d1c8083dc4429333e",
+    "url": "/js/chunk-vendors.76a774d1.js"
+  },
+  {
+    "revision": "cec554725bb06939b502ddffdae64d32",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
