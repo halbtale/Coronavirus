a4d756c0e8cac127f20ac21f2101ffb2473ab5de
Alberto <alberto.ice04@gmail.com>
2021-01-04 14:42:32 +0100

    Updated data source

--- src/store/index.js
+++ src/store/index.js
@@ -1,73 +1,29 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-const domparser = new DOMParser();
-const storage = window.localStorage;
+import axios from 'axios'
+
+const API_NATIONAL_DATA_URL = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json'
+const API_REGIONS_DATA_URL = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni-latest.json'
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
 
 Vue.use(Vuex);
 
-async function getData(url) {
-	// Get webpage HTML
-	const rawData = await fetch(url);
-	const data = await rawData.text();
-	const doc = domparser.parseFromString(data, 'text/html');
-
-	// Get total counts
-	const mainCounterSpans = doc.querySelectorAll('.maincounter-number span');
-	const infected = mainCounterSpans[0].innerText;
-	const deaths = mainCounterSpans[1].innerText;
-
-	return { infected, deaths };
-}
-
 export default new Vuex.Store({
 	state: {
-		infectedTotal: 0,
-		deathsTotal: 0,
-		infectedItaly: 0,
-		deathsItaly: 0
+		nationalData: null,
+		regionsData: null
 	},
 	mutations: {
 		setNewStats(state, payload) {
-			state.infectedTotal = payload.infectedTotal;
-			state.deathsTotal = payload.deathsTotal;
-			state.infectedItaly = payload.infectedItaly;
-			state.deathsItaly = payload.deathsItaly;
+			state.nationalData = payload.nationalData;
+			state.regionsData = payload.regionsData;
 		}
 	},
 	actions: {
 		async load({ commit }) {
-			// Set local stats for fast preview
-			const localStats = {
-				infectedTotal: storage.getItem('infectedTotal'),
-				deathsTotal: storage.getItem('deathsTotal'),
-				infectedItaly: storage.getItem('infectedItaly'),
-				deathsItaly: storage.getItem('deathsItaly')
-			};
-
-			if (
-				localStats.infectedTotal &&
-				localStats.deathsTotal &&
-				localStats.infectedItaly &&
-				localStats.deathsItaly
-			) {
-				commit('setNewStats', localStats);
-			}
-
-			// Fetch new data
-			const { deaths: deathsTotal, infected: infectedTotal } = await getData(
-				'https://cors-anywhere.herokuapp.com/https://www.worldometers.info/coronavirus/'
-			);
-
-			const { deaths: deathsItaly, infected: infectedItaly } = await getData(
-				'https://cors-anywhere.herokuapp.com/https://www.worldometers.info/coronavirus/country/italy/'
-			);
-
-			storage.setItem('infectedTotal', infectedTotal);
-			storage.setItem('deathsTotal', deathsTotal);
-			storage.setItem('infectedItaly', infectedItaly);
-			storage.setItem('deathsItaly', deathsItaly);
-
-			commit('setNewStats', { infectedTotal, deathsTotal, infectedItaly, deathsItaly });
+			const nationalData = (await axios.get(PROXY_URL + API_NATIONAL_DATA_URL)).data;
+			const regionsData = (await axios.get(PROXY_URL + API_REGIONS_DATA_URL)).data;
+			commit('setNewStats', {nationalData, regionsData})
 		}
 	},
 	modules: {}
--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,71 +1,61 @@
 <template>
-    <div id="counter">
-        <h1 class="title">
-            Coronavirus
-            <br />Live stats
-        </h1>
+	<div id="counter">
+		<h1 class="title">
+			Coronavirus
+			<br />Live stats
+		</h1>
 
-        <p class="caption">Totale infetti nel mondo</p>
-        <h2 class="number">{{ infectedTotal }}</h2>
-
-        <p class="caption">Totale morti nel mondo</p>
-        <h2 class="number">{{ deathsTotal }}</h2>
-
-        <p class="caption">Totale infetti in Italia</p>
-        <h2 class="number">{{ infectedItaly }}</h2>
-
-        <p class="caption">Totale morti in Italia</p>
-        <h2 class="number">{{ deathsItaly }}</h2>
-    </div>
+		<p class="caption">Nuovi infetti in italia</p>
+		<h2 class="number">{{ nationalInfectsIncrement }}</h2>
+	</div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 
 export default {
-    name: "App",
-    computed: {
-        ...mapState([
-            "infectedTotal",
-            "deathsTotal",
-            "infectedItaly",
-            "deathsItaly"
-        ])
-    }
+	name: "App",
+	computed: {
+		...mapState(["nationalData", "regionsData"]),
+		nationalInfectsIncrement() {
+			if (!this.nationalData) return "LOADING...";
+			return this.nationalData[0]["nuovi_positivi"];
+		},
+	},
 };
 </script>
 
 <style lang="scss" scoped>
 #counter {
-    background-image: linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),
-        url("../assets/background.jpg");
-    background-size: cover;
-    background-position: center;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    color: white;
-    align-items: center;
-    justify-content: center;
-    font-family: "Open Sans";
+	background-image: linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)),
+		url("../assets/background.jpg");
+	background-size: cover;
+	background-position: center;
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	color: white;
+	align-items: center;
+	justify-content: center;
+	font-family: "Open Sans";
 }
 .title {
-    font-family: "Coda";
-    font-size: 4em;
-    text-align: center;
-    margin-bottom: 8vh;
-    margin-top: 5vh;
-    @media only screen and (max-width: 700px) {
-        font-size: 2.3em;
-        margin-top: 30px;
-    }
+	font-family: "Coda";
+	font-size: 4em;
+	text-align: center;
+	margin-bottom: 8vh;
+	margin-top: 5vh;
+	@media only screen and (max-width: 700px) {
+		font-size: 2.3em;
+		margin-top: 30px;
+	}
 }
 .caption {
-    font-size: 1.1em;
+	font-size: 1.1em;
 }
 .number {
-    font-size: 2.5em;
-    margin-bottom: 3vh;
+	font-size: 2.5em;
+	margin-bottom: 3vh;
 }
 </style>
